2ae04ca72c89de1af7c43294a503377974915447
Ashu111144 <ashutoshkumar111144@gmail.com>
2022-03-08 18:45:24 +0530

    Basic Stack implement

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -15,17 +15,65 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var screenHeight;
+  var screenWidth;
+
   @override
   Widget build(BuildContext context) {
-    return Stack(children: const [
-      Image(
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(children: [
+      const Image(
         image: AssetImage('assets/homepage/homeScreen_background.png'),
         fit: BoxFit.cover,
         height: double.infinity,
         width: double.infinity,
         alignment: Alignment.center,
       ),
-
+      Positioned(
+        top: screenHeight * .25,
+        bottom: screenHeight * .25,
+        left: screenWidth * .2,
+        right: screenWidth * .6,
+        child: Stack(children: [
+          const Image(
+            image: AssetImage('assets/homepage/menu_background.png'),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Image(
+                  image: AssetImage('assets/homepage/menu_button.png'),
+                ),
+                Image(
+                  image: AssetImage('assets/homepage/menu_button.png'),
+                ),
+                Image(
+                  image: AssetImage('assets/homepage/menu_button.png'),
+                ),
+                Image(
+                  image: AssetImage('assets/homepage/menu_button.png'),
+                ),
+                Image(
+                  image: AssetImage('assets/homepage/menu_button.png'),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+      Positioned(
+        top: screenHeight * .25,
+        bottom: screenHeight * .25,
+        left: screenWidth * .6,
+        right: screenWidth * .2,
+        child: const Image(
+          image: AssetImage('assets/homepage/menu_background.png'),
+        ),
+      ),
     ]);
   }
 }
