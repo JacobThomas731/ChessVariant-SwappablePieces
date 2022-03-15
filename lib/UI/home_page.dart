daa665b1b566f95784091058c66093dd33b5d5d6
Ashu111144 <ashutoshkumar111144@gmail.com>
2022-03-15 20:23:42 +0530

    HomePage UI rebuild

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -4,117 +4,227 @@ import 'package:flutter/material.dart';
 import 'package:chess_variant_swappable_pieces/chess_board.dart';
 
 class HomePage extends StatefulWidget {
-  final title;
-
-  const HomePage(this.title, {Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  var screenHeight;
-  var screenWidth;
-
   @override
   Widget build(BuildContext context) {
-    screenHeight = MediaQuery.of(context).size.height;
-    screenWidth = MediaQuery.of(context).size.width;
-
-
-    return Stack(children: [
-      const Image(
-        image: AssetImage('assets/homepage/homeScreen_background.png'),
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
-        alignment: Alignment.center,
-      ),
-      Positioned(
-        top: screenHeight * .2,
-        bottom: screenHeight * .2,
-        left: screenWidth * .1,
-        right: screenWidth * .70,
-        child: Stack(children: [
-          const Image(
-            image: AssetImage('assets/homepage/menu_background.png'),
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Board()),
-                    );
-                  },
-                  child: const Image(
-                    image: AssetImage('assets/homepage/findGame_button.png'),
-                    // height: 65,
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    var topSpace = screenHeight * .2;
+    var gapSpace = screenWidth * .1;
+    var menuPadding = screenHeight * screenWidth * 0.00002;
+    var menuWidth = screenWidth * .2;
+    var menuHeight = menuWidth * 1.5;
+    var totalMenuBar = 5;
+    var menuBarHeight = (menuHeight - 6 * menuPadding) / totalMenuBar;
+    var menuBarWidth = menuWidth;
+    var menuTextColor = const Color(0xF21D150F);
+    var menuBgColor = const Color(0x8B4B2B21);
+    var menuBarBgColor = const Color(0x513D1B17);
+    var menuText1 = 'Play';
+    var menuText2 = 'Lobby';
+    var menuText3 = 'Friends';
+    var menuText4 = 'Rules';
+    var menuText5 = 'Setting';
+    var menuTextFontSize = screenHeight * screenWidth * .00003;
+    var titleWidth = screenWidth * .5;
+    var titleHeight = titleWidth * .3;
+    var registrationBgHeight = titleHeight * .3;
+    var registrationBgWidth = titleWidth;
+    var textColor2 = const Color(0xdac2a273);
+    var title1Size = screenHeight * screenWidth * .00003;
+    var title2Size = screenHeight * screenWidth * .00005;
+    var registrationTextSize = screenHeight * screenWidth * .00002;
+    return Material(
+      type: MaterialType.transparency,
+      child: Stack(children: [
+        const Image(
+          image: AssetImage('assets/homepage/homeScreen_background.png'),
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+        Positioned(
+          top: topSpace,
+          left: gapSpace,
+          right: screenWidth - menuWidth - gapSpace,
+          child: Stack(children: [
+            Container(
+              color: menuBarBgColor,
+              height: menuHeight,
+              width: menuWidth,
+            ),
+            Padding(
+              padding: EdgeInsets.all(menuPadding),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Board()),
+                      );
+                    },
+                    child: Container(
+                      color: menuBgColor,
+                      height: menuBarHeight,
+                      width: menuBarWidth,
+                      child: Center(
+                        child: Text(
+                          menuText1,
+                          style: TextStyle(
+                              color: menuTextColor, fontSize: menuTextFontSize),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Container(
+                    height: menuPadding,
+                  ),
+                  Container(
+                    color: menuBgColor,
+                    height: menuBarHeight,
+                    width: menuBarWidth,
+                    child: Center(
+                      child: Text(
+                        menuText2,
+                        style: TextStyle(
+                          color: menuTextColor,
+                          fontSize: menuTextFontSize,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: menuPadding,
+                  ),
+                  Container(
+                    color: menuBgColor,
+                    height: menuBarHeight,
+                    width: menuBarWidth,
+                    child: Center(
+                        child: Text(menuText3,
+                            style: TextStyle(
+                                color: menuTextColor,
+                                fontSize: menuTextFontSize))),
+                  ),
+                  Container(
+                    height: menuPadding,
+                  ),
+                  Container(
+                    color: menuBgColor,
+                    height: menuBarHeight,
+                    width: menuBarWidth,
+                    child: Center(
+                        child: Text(menuText4,
+                            style: TextStyle(
+                                color: menuTextColor,
+                                fontSize: menuTextFontSize))),
+                  ),
+                  Container(
+                    height: menuPadding,
+                  ),
+                  Container(
+                    color: menuBgColor,
+                    height: menuBarHeight,
+                    width: menuBarWidth,
+                    child: Center(
+                        child: Text(menuText5,
+                            style: TextStyle(
+                                color: menuTextColor,
+                                fontSize: menuTextFontSize))),
+                  ),
+                ],
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Image(
-                  image: AssetImage('assets/homepage/joinLobby_button.png'),
-                  // height: 40,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Image(
-                  image:
-                      AssetImage('assets/homepage/playwithfriend_button.png'),
-                  // height: 40,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Image(
-                  image: AssetImage('assets/homepage/rules_button.png'),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Image(
-                  image: AssetImage('assets/homepage/settings_button.png'),
-                ),
+            ),
+          ]),
+        ),
+        Positioned(
+          top: screenHeight * .2,
+          left: screenWidth * .4,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: titleHeight,
+                    width: titleWidth,
+                    color: menuBarBgColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: menuPadding),
+                          child: Text(
+                            'multiplayer chess variant:',
+                            style: TextStyle(
+                              fontSize: title1Size,
+                              color: textColor2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: menuPadding),
+                          child: Text(
+                            'swappable pieces',
+                            style: TextStyle(
+                              fontSize: title2Size,
+                              color: textColor2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: menuPadding,
+                  ),
+                  Container(
+                    height: registrationBgHeight,
+                    width: registrationBgWidth,
+                    color: menuBgColor,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: registrationTextSize,
+                            color: textColor2,
+                          ),
+                        ),
+                        Text(
+                          '|',
+                          style: TextStyle(
+                            fontSize: registrationTextSize,
+                            color: textColor2,
+                          ),
+                        ),
+                        Text(
+                          'Sign-in',
+                          style: TextStyle(
+                            fontSize: registrationTextSize,
+                            color: textColor2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ],
           ),
-        ]),
-      ),
-      Positioned(
-        top: screenHeight * .2,
-        bottom: screenHeight * .2,
-        left: screenWidth * .45,
-        right: screenWidth * .1,
-        child: Stack(
-          children: [
-            Column(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Image(
-                    image: AssetImage('assets/homepage/title.png'),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Image(
-                    image:
-                        AssetImage('assets/homepage/register_sign_in_bg.png'),
-                  ),
-                ),
-              ],
-            ),
-          ],
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
---
+++ lib/UI/optionButttonUI/optionButton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OptionButton extends StatefulWidget {
+  const OptionButton({Key? key}) : super(key: key);
+
+  @override
+  _OptionButtonState createState() => _OptionButtonState();
+}
+
+class _OptionButtonState extends State<OptionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
